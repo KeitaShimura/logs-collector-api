8d81310f7387ddb4c0838e682104fac79f664cc2
KeitaShimura <keitashimura2023@gmail.com>
2025-04-15 23:01:05 +0900

    refactor: reorder code and update comments

--- internal/testutil/mock_logger.go
+++ internal/testutil/mock_logger.go
@@ -17,25 +17,25 @@ type MockLogger struct {
 	Errors []LogEntry
 }
 
-// SetLevel はロガーのレベルを設定する（実際には使用しない）
+// SetLevel は動的にログレベルを変更する（実際には使用しない）
 func (m *MockLogger) SetLevel(_ slog.Level) {}
 
-// Info は情報ログを記録する
-func (m *MockLogger) Info(msg string, args ...any) {
-	m.Infos = append(m.Infos, LogEntry{Msg: msg, Args: args})
-}
-
-// Debug はデバッグログを記録する
+// Debug はデバッグ用の詳細ログを出力する
 func (m *MockLogger) Debug(msg string, args ...any) {
 	m.Infos = append(m.Infos, LogEntry{Msg: msg, Args: args})
 }
 
-// Warn は警告ログを記録する
+// Info は情報ログを出力する
+func (m *MockLogger) Info(msg string, args ...any) {
+	m.Infos = append(m.Infos, LogEntry{Msg: msg, Args: args})
+}
+
+// Warn は警告ログを出力する
 func (m *MockLogger) Warn(msg string, args ...any) {
 	m.Warns = append(m.Warns, LogEntry{Msg: msg, Args: args})
 }
 
-// Error はエラーログを記録する
+// Error はエラーログを出力する（nil エラーも考慮）
 func (m *MockLogger) Error(msg string, err error, args ...any) {
 	if err != nil {
 		args = append(args, slog.String("error", err.Error()))
