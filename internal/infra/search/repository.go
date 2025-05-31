a3173bf51e6faf2fe0259690316d6f2ecd91ad0d
KeitaShimura <keitashimura2023@gmail.com>
2025-05-31 17:30:55 +0900

    refactor: add LogIndexer interface and switch to esapi.Transport

--- internal/infra/search/repository.go
+++ internal/infra/search/repository.go
@@ -2,6 +2,7 @@ package search
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -17,16 +18,25 @@ var ErrIndexingFailed = errors.New("indexing to Elasticsearch failed")
 // ESClient は Elasticsearch クライアントのインターフェース（テスト用抽象化）
 type ESClient interface {
 	Index(index string, body *bytes.Reader, o ...func(*esapi.IndexRequest)) (*esapi.Response, error)
+	Do(req *esapi.IndexRequest) (*esapi.Response, error)
+}
+
+// LogIndexer は Elasticsearch にログを登録するためのインターフェース
+type LogIndexer interface {
+	IndexLog(index string, logData map[string]interface{}) error
 }
 
 // LogSearcher は Elasticsearch へのログ登録用構造体
 type LogSearcher struct {
-	client ESClient
+	client esapi.Transport
 	log    logger.Logger
 }
 
+// インターフェース実装の確認
+var _ LogIndexer = (*LogSearcher)(nil)
+
 // NewLogSearcher は LogSearcher を作成する
-func NewLogSearcher(client ESClient, log logger.Logger) *LogSearcher {
+func NewLogSearcher(client esapi.Transport, log logger.Logger) *LogSearcher {
 	return &LogSearcher{client: client, log: log}
 }
 
@@ -38,8 +48,14 @@ func (ls *LogSearcher) IndexLog(index string, logData map[string]interface{}) er
 		return fmt.Errorf("failed to marshal log (index: %s): %w", index, err)
 	}
 
+	//nolint:exhaustruct // 未使用フィールドはデフォルト値で問題ないため省略
+	req := esapi.IndexRequest{
+		Index: index,
+		Body:  bytes.NewReader(data),
+	}
+
 	// Elasticsearch にデータを送信
-	res, err := ls.client.Index(index, bytes.NewReader(data))
+	res, err := req.Do(context.Background(), ls.client)
 	if err != nil {
 		return fmt.Errorf("failed to send index request (index: %s): %w", index, err)
 	}
